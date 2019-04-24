a1497d8d5664d57f401c005dbc0ea768f105e24b
Anisha Keshavan <anisha@childmind.org>
2019-04-24 15:00:51 -0700

    enh: respecting the timeUnit in the schema on the dashboard

--- src/components/Visualization/ActivityDash/ActivityDash.vue
+++ src/components/Visualization/ActivityDash/ActivityDash.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <div v-for="(itemData, itemUrl) in data" :key="itemUrl">
+    <div v-for="(itemData, itemUrl) in cleanedData" :key="itemUrl">
       <ItemPlot :data="itemData" :itemUrl="itemUrl" />
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+import jsonld from 'jsonld/dist/jsonld.min';
 import ItemPlot from '../ItemPlot';
+import moment from 'moment';
 
 export default {
   name: 'ActivityDash',
@@ -24,11 +27,47 @@ export default {
   },
   data() {
     return {
-
+      metadata: {},
     };
   },
-  methods: {
+  computed: {
+    activityTimeUnit() {
+      if (!_.isEmpty(this.metadata)) {
+        if (this.metadata[0]['https://schema.repronim.org/timeUnit']) {
+          return this.metadata[0]['https://schema.repronim.org/timeUnit'][0]['@value']
+        }
+        return null;
+      }
+      return null;
+    },
+    cleanedData() {
+      const cleanedData = {};
+      switch (this.activityTimeUnit) {
+        case 'yearmonthdate':
+          _.map(this.data, (itemData, itemURI) => {
+            cleanedData[itemURI] = this.getByDate(itemData);
+          });
+          return cleanedData;
 
+        default:
+          return this.data;
+      }
+    },
+  },
+  methods: {
+    getActivityMetadata() {
+      jsonld.expand(this.activityUrl).then((resp) => {
+        this.metadata = resp;
+      });
+    },
+    getByDate(data) {
+      const dayGroups = _.groupBy(data, d => moment(d.time_of_response).startOf('day').toISOString());
+      // const output = [];
+      return _.map(dayGroups, d => _.last(d));
+    },
+  },
+  mounted() {
+    this.getActivityMetadata();
   },
 };
 </script>
